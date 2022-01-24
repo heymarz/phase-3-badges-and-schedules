0a21c4ecd4db34a2d0e811481ab67d1de6707f2c
Marlena Tsang <Marlena345@gmail.com>
2022-01-23 23:07:54 -0500

    commit tree 054c8069fec9939e42e2977426a7961e4a5e817e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |person|
+    "Hello, my name is #{person}."
+  end
+end
+
+def assign_rooms(persons)
+  persons.map.with_index(1) do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).map do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).map do |room|
+    puts room
+  end
+end
